ecbf0ce4f698a89c2d7216504e82cf32e55cca6f
doanndev <doann.dev@gmail.com>
2024-12-06 17:48:57 +0700

    Fix Bug: Cant import SVG file as a component & Config Webpack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Icon from "@/components/Icon";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <h1 className="text-4xl font-bold text-center text-blue-600 mt-8">
       Carefree Social NetWork
     </h1>
+    <Icon />
+    </>
   );
 }
---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import IconChatGPT from '@/assets/svgs/ChatGPT.svg';
+
+type Props = {}
+
+const Icon = (props: Props) => {
+  return (
+    <IconChatGPT />
+  )
+}
+
+export default Icon
